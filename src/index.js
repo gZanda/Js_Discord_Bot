fc220982a1a974dad8d7228b7227d355bb15799e
gZanda <gabrielfzanda@gmail.com>
2024-01-14 00:41:58 -0300

    Feat: Bot is super SMART now

--- src/index.js
+++ src/index.js
@@ -23,55 +23,61 @@ client.on('messageCreate', async (message)=>{
     if (message.author.bot) return;
 
     else if (message.content.toLowerCase() === 'oi'){
-        message.channel.send('**Oiii, Vc é viado!**  (  ≽^•⩊•^≼  )');	
+        const embed = new EmbedBuilder().setDescription(`**≽ ^ • ⩊ • ^ ≼ \n \n Oiii ${message.author}, vc é V I A D O**`).setColor("Purple");
+        message.channel.send({embeds: [embed]});	
     }
 
     else if (message.mentions.has(client.user) && message.content.toLowerCase().includes("oi")) {
         // Respond to the mention
-        //const mentioningUser = message.mentions.users.first();
-        message.channel.send(`**Oi ${message.author}, o q vc quer seu viado ?**  (  ≽^•⩊•^≼  )`);
+        const embed = new EmbedBuilder().setDescription(`**≽ ^ • ⩊ • ^ ≼ \n \n Oi ${message.author}, o q vc quer seu viado ?**`).setColor("Purple");
+        message.channel.send({embeds: [embed]});
     }
 
     else if (message.mentions.has(client.user) && message.content.toLowerCase().includes("reação")) {
         // Respond to the mention
         const response = await axios.get(`https://g.tenor.com/v1/random?q=cat_reaction&key=${process.env.TENOR_KEY}&limit=3&media_filter=minimal`)
         const gifUrl = response.data.results[0].media[0].gif.url;
-        const embed = new EmbedBuilder().setTitle("≽ ^ • ⩊ • ^ ≼ \n Minha honesta reação:").setImage(gifUrl).setColor("Purple");
+        const embed = new EmbedBuilder().setTitle("**≽ ^ • ⩊ • ^ ≼ \n \n Minha honesta reação:**").setImage(gifUrl).setColor("Purple");
         message.channel.send({embeds: [embed]});
     }
 
     else if (message.mentions.has(client.user) && message.content.toLowerCase().includes("porra") || message.content.toLowerCase().includes("poha")) {
-        const embed = new EmbedBuilder().setTitle("**≽ ^ • ⩊ • ^ ≼ \n \n SIM! \n NÓS DA O CU PORRA!**").setImage("https://pbs.twimg.com/media/F8fLk3BXYAAp6KT.jpg").setColor("Purple");
+        const embed = new EmbedBuilder().setTitle("**≽ ^ • ⩊ • ^ ≼ \n \n NÓS DA O CU PORRA**").setImage("https://pbs.twimg.com/media/F8fLk3BXYAAp6KT.jpg").setColor("Purple");
         message.channel.send({embeds: [embed]});
     }
 
-    else message.channel.send(`**≽ ^ • ⩊ • ^ ≼  \n Ops! Parece que um viado me mencinou atoa.**  `);
+    else if (message.attachments.size > 0) {
+        const response = await axios.get(`https://g.tenor.com/v1/random?q=cat_reaction&key=${process.env.TENOR_KEY}&limit=3&media_filter=minimal`);
+        const gifUrl = response.data.results[0].media[0].gif.url;
+        const embed = new EmbedBuilder().setTitle("**≽ ^ • ⩊ • ^ ≼ \n \n Minha honesta reação:**").setImage(gifUrl).setColor("Purple");
+        message.channel.send({embeds: [embed]});
+    }
+
+    else {
+        const embed = new EmbedBuilder().setTitle("**≽ ^ • ⩊ • ^ ≼ \n \n NÃO ENTENDI PORRA NENHUMA**").setImage("https://pbs.twimg.com/media/F4ZjUFWWgAA5oul.jpg").setColor("Purple");
+        message.channel.send({embeds: [embed]});
+    }
 });
 
 // Event Listener -> Slash Commands -> DEFINE HERE WHAT THE BOT GONNA DO WITH THE COMMAND
-client.on('interactionCreate', (interaction)=>{
+client.on('interactionCreate',async (interaction)=>{
     if (!interaction.isChatInputCommand()) return;
 
-    else if (interaction.commandName === 'ping'){
-        interaction.reply('**Pong!**  (  ≽^•⩊•^≼  )');
-    }
-
     else if (interaction.commandName === 'aviadar'){
-        interaction.reply('**Agora vc é V I A D O!**  (  ≽^•⩊•^≼  )');
+        const response = await axios.get(`https://g.tenor.com/v1/random?q=congratulations_meme&key=${process.env.TENOR_KEY}&limit=3&media_filter=minimal`);
+        const gifUrl = response.data.results[0].media[0].gif.url;
+        const embed = new EmbedBuilder().setTitle(`**≽ ^ • ⩊ • ^ ≼ \n \n Parabéns 🥳 Agora você é V I A D O!**`).setImage(gifUrl).setColor("Purple");
+        interaction.reply({embeds: [embed]});
     }
 
     else if (interaction.commandName === 'ofender'){
         const user = interaction.options.getUser('user');
-        interaction.reply(`**Vai se foder ${user}!**🖕​🤬  (  ≽^•⩊•^≼  )`);
-    }
-
-    else if (interaction.commandName === 'embed'){
-        const embed = new EmbedBuilder().setTitle("Tittle Sample").setDescription("Description Sample").setColor("Purple")
+        const embed = new EmbedBuilder().setDescription(`**≽ ^ • ⩊ • ^ ≼ \n \n Vai se foder ${user}!🖕​🤬**`).setColor("Purple");
         interaction.reply({embeds: [embed]});
     }
 
     else if (interaction.commandName === 'gato'){
-        const embed = new EmbedBuilder().setTitle("**≽ ^ • ⩊ • ^ ≼**").setImage("https://ih0.redbubble.net/image.4583816833.9469/raf,360x360,075,t,fafafa:ca443f4786.jpg").setColor("Purple");
+        const embed = new EmbedBuilder().setTitle("**≽ ^ • ⩊ • ^ ≼ \n \n Olhem esse gato de B O S T A 👇**").setImage("https://ih0.redbubble.net/image.4583816833.9469/raf,360x360,075,t,fafafa:ca443f4786.jpg").setColor("Purple");
         interaction.reply({embeds: [embed]});
     }
 
